7d0284fe5e16816bf4b5ac7949c32c4829c2d147
Hristo Spasov <hristo@arc-global.org>
2018-06-12 10:47:41 +0300

    Get sample locations from database to front-end

--- gmapsapitask.php
+++ gmapsapitask.php
@@ -2,31 +2,33 @@
 /**
 *Plugin Name: Google maps API task
 **/
+  $locations = $wpdb->get_results("SELECT * FROM locations LIMIT 2;");
 
-    function gmaps_api_task()
-    {
-        $content = '<div id="map" style="height: 400px; width: 100%;"></div>';
-    ?>
-    <script>
-      function initMap() {
-        var uluru = {lat: -25.363, lng: 31.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtClkLrQakqs3VuJVRm_dxl66z6RLJsqo&callback=initMap">
-    </script>
-    <?php
-        return $content;
+  function gmaps_api_task()
+  {
+    global $locations;
+    $content = '<div id="map" style="height: 400px; width: 100%;"></div>';
+  ?>
+  <script>
+    <?php echo 'var locations_array = '.json_encode($locations).';';?>;   
+    console.log(locations_array);
+    function initMap() {
+      var uluru = {lat: -25.363, lng: 31.044};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
     }
-    add_shortcode('gmapsapitask', 'gmaps_api_task');
-
-
+  </script>
+  <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA9HxwNfmUEyBe1F2NV72n2PJRxy81ss9Q&callback=initMap">
+  </script>
+  <?php
+    return $content;
+  }
+  add_shortcode('gmapsapitask', 'gmaps_api_task');
 ?>
