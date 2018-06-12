07b098295d1d229542bf23a59d43f878cc24a99a
Hristo Spasov <hristo@arc-global.org>
2018-06-12 11:16:20 +0300

    Display locations, stored in the database, on the map. Display address onb marker click.

--- gmapsapitask.php
+++ gmapsapitask.php
@@ -2,7 +2,7 @@
 /**
 *Plugin Name: Google maps API task
 **/
-  $locations = $wpdb->get_results("SELECT * FROM locations LIMIT 2;");
+  $locations = $wpdb->get_results('SELECT address, lat, lng FROM locations;');
 
   function gmaps_api_task()
   {
@@ -10,22 +10,32 @@
     $content = '<div id="map" style="height: 400px; width: 100%;"></div>';
   ?>
   <script>
-    <?php echo 'var locations_array = '.json_encode($locations).';';?>;   
-    console.log(locations_array);
+    <?php echo 'var locations = '.json_encode($locations).';';?>;   
     function initMap() {
-      var uluru = {lat: -25.363, lng: 31.044};
+      var usa = {lat: 37.275, lng: -95.655};
       var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
+        zoom: 3,
+        center: usa
       });
-      var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
+      locations.forEach(location => {
+        var infoWindow = new google.maps.InfoWindow({
+          content: location.address
+        });
+        var marker = new google.maps.Marker({
+          position: {
+            lat: parseFloat(location.lat),
+            lng: parseFloat(location.lng)
+          },
+          map: map
+        });
+        marker.addListener('click', () => {
+          infoWindow.open(map, marker);
+        });
       });
     }
   </script>
   <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA9HxwNfmUEyBe1F2NV72n2PJRxy81ss9Q&callback=initMap">
+    src='https://maps.googleapis.com/maps/api/js?key=AIzaSyA9HxwNfmUEyBe1F2NV72n2PJRxy81ss9Q&callback=initMap'>
   </script>
   <?php
     return $content;
